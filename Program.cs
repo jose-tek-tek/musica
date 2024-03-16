4ab5b41aaf87b2c2cb5979402eef29aa81cff74b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-15 22:40:25 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿Console.BackgroundColor =ConsoleColor.DarkBlue;
+Console.ForegroundColor =ConsoleColor.DarkGreen;
+Console.WriteLine("Proteção de Tela - Tarcísio do Acordeon");
+Console.ResetColor();
+Console.WriteLine(@"Ei, ei, ei
+Oh oh oh
+
+Eu já tentei de tudo pra tirar você de vez da minha vida
+Mas não dá
+
+E toda vez que eu lembro de você
+Dou outro gole na bebida
+Sem notar
+
+Embriagado, desiludido
+O som no talo
+Ouvindo Tarcísio
+
+Quando bate a saudade
+E vejo as mensagens e a foto dela
+Na minha proteção de tela
+E tome cana na minha goela");
+
+Console.BackgroundColor =ConsoleColor.DarkBlue;
+Console.ForegroundColor =ConsoleColor.DarkGreen;
+Console.WriteLine(@"Vontade de ligar
+Mas sou bloqueado de tudo na vida dela
+Só resta a proteção de tela
+E tome cana na minha goela
+");
+Console.ResetColor();
+
+Console.WriteLine(@"É diferente dos iguais
+Oh oh
+
+Eu já tentei de tudo pra tirar você de vez da minha vida
+Mas não dá
+
+E toda vez que eu lembro de você
+Dou outro gole na bebida
+Sem notar
+
+Embriagado, desiludido
+O som no talo
+Ouvindo Tarcísio
+
+Quando bate a saudade
+E vejo as mensagens e a foto dela
+Na minha proteção de tela
+E tome cana na minha goela");
+
+Console.BackgroundColor =ConsoleColor.DarkBlue;
+Console.ForegroundColor =ConsoleColor.DarkGreen;
+Console.WriteLine(@"Vontade de ligar
+Mas sou bloqueado de tudo na vida dela
+Só resta a proteção de tela
+E tome cana na minha goela
+");
+Console.ResetColor();
+
+Console.WriteLine(@"Quando bate a saudade
+E vejo as mensagens e a foto dela
+Na minha proteção de tela
+E tome cana na minha goela");
+
+Console.BackgroundColor =ConsoleColor.DarkBlue;
+Console.ForegroundColor =ConsoleColor.DarkGreen;
+Console.WriteLine(@"Vontade de ligar
+Mas sou bloqueado de tudo na vida dela
+Só resta a proteção de tela
+E tome cana na minha goela
+");
+Console.ResetColor();
+
+Console.WriteLine(@"Vai na pegada do gordinho
+Oh oh oh");
+
