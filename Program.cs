70f8953a18b419cb0ff34690d6c060021523b206
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-18 20:21:04 -0300

    Modificado

--- Program.cs
+++ Program.cs
@@ -2,7 +2,11 @@
 Console.ForegroundColor =ConsoleColor.DarkGreen;
 Console.WriteLine("Proteção de Tela - Tarcísio do Acordeon");
 Console.ResetColor();
-Console.WriteLine(@"Ei, ei, ei
+
+Console.WriteLine("");
+
+Console.WriteLine(@"
+Ei, ei, ei
 Oh oh oh
 
 Eu já tentei de tudo pra tirar você de vez da minha vida
@@ -19,18 +23,20 @@ Ouvindo Tarcísio
 Quando bate a saudade
 E vejo as mensagens e a foto dela
 Na minha proteção de tela
-E tome cana na minha goela");
+E tome cana na minha goela
+");
 
 Console.BackgroundColor =ConsoleColor.DarkBlue;
 Console.ForegroundColor =ConsoleColor.DarkGreen;
-Console.WriteLine(@"Vontade de ligar
+Console.Write(@"Vontade de ligar
 Mas sou bloqueado de tudo na vida dela
 Só resta a proteção de tela
 E tome cana na minha goela
 ");
 Console.ResetColor();
 
-Console.WriteLine(@"É diferente dos iguais
+Console.WriteLine(@"
+É diferente dos iguais
 Oh oh
 
 Eu já tentei de tudo pra tirar você de vez da minha vida
@@ -47,31 +53,36 @@ Ouvindo Tarcísio
 Quando bate a saudade
 E vejo as mensagens e a foto dela
 Na minha proteção de tela
-E tome cana na minha goela");
+E tome cana na minha goela
+");
 
 Console.BackgroundColor =ConsoleColor.DarkBlue;
 Console.ForegroundColor =ConsoleColor.DarkGreen;
-Console.WriteLine(@"Vontade de ligar
+Console.Write(@"Vontade de ligar
 Mas sou bloqueado de tudo na vida dela
 Só resta a proteção de tela
 E tome cana na minha goela
 ");
 Console.ResetColor();
 
-Console.WriteLine(@"Quando bate a saudade
+Console.WriteLine(@"
+Quando bate a saudade
 E vejo as mensagens e a foto dela
 Na minha proteção de tela
-E tome cana na minha goela");
+E tome cana na minha goela
+");
 
 Console.BackgroundColor =ConsoleColor.DarkBlue;
 Console.ForegroundColor =ConsoleColor.DarkGreen;
-Console.WriteLine(@"Vontade de ligar
+Console.Write(@"Vontade de ligar
 Mas sou bloqueado de tudo na vida dela
 Só resta a proteção de tela
 E tome cana na minha goela
 ");
 Console.ResetColor();
 
-Console.WriteLine(@"Vai na pegada do gordinho
-Oh oh oh");
+Console.WriteLine(@"
+Vai na pegada do gordinho
+Oh oh oh
+");
 
